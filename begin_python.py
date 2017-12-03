e4c68dc4cc501e6215c3805d065a52f8cb6d811a
ajay291491 <ajay291491@gmail.com>
2017-12-04 07:22:52 +0800

    updating python doc

--- begin_python.py
+++ begin_python.py
@@ -25,8 +25,8 @@
 # | print ("Sum of", num_one, "and", num_two, "is :", total)
 # 
 # * This is how you will make multiline comments 
-"""" here I am going to make a multi line comment
-It can be about anything """
+# | """" here I am going to make a multi line comment
+# | It can be about anything """
 #
 # * Single line comment 
 # | # This is a single line comment
@@ -57,16 +57,21 @@ It can be about anything """
 # | country = "singapore"
 # | print name + str(32) + " aged lives in " + country
 #
-# * Advanced string concatination using placeholder '%s'
-# Rather than using '+' sign you can use '%s' to declare the variables in a statement and can define all variables in the last by %(var_one, var_two, etc)
+# * Advanced string concatination using placeholders
+# Rather than using '+' sign you can use 'place holders' to declare the variables in a statement and can define all variables in the last by %(var_one, var_two, etc)
 # This will pick the variable in the order you have given in the %() array.
+#
+# There are mainly two place holders 
+# %s    - To print string
+# %d    - To print digits
 #
 # Example : You can see an example for advnaced string concatination using %s below
 #
 # | fruit_one = "apple"
 # | fruit_two = "orange"
 # | fruit_three = "strawberry"
-# | print "There are three fruits in the basket %s %s %s" %(fruit_one, fruit_three, fruit_two)
+# | total_fruits = 3
+# | print "There are %d fruits in the basket %s %s %s" %(total_fruits, fruit_one, fruit_three, fruit_two)
 # | print "These %s and %s fruits can be taken to schoool, but %s can only to hospital" %(fruit_two, fruit_three, fruit_one)
 #
 # NOTE : Make sure the number of '%s' you have in the sting and vriables with in %( ) should be same, else the compilation will fail
@@ -215,4 +220,129 @@ It can be about anything """
 #
 # Pending : Chapter 3 9/11
 #
+#-------------------------------------------------------------------------------------------------------------
+# Chapter 04 - Functions 
+#-------------------------------------------------------------------------------------------------------------
 #
+# * Functions 
+# Rather than writing the whole code in a single script best way is to devide each chunk of code as 'functions' which can do certian purpose. 
+# When you start writing codes with functions, you will be able to re-use the same function again and again in the same code which will help you to reduce the total code. 
+# This will also bringings easiness in managing the script 
+#
+# There are few things which are mandatory while defining the function 
+# def                   - def should be mentioned when you define a function
+# function_name         - name of the function 
+# (list_of_inputs)      - List of inputs to the functions
+# :                     - column must be there after the inputs and before the contents starts 
+# """ comment """"      - Comment about the functionality 
+#
+# Syntax : def function_name(list_of_inputs):                               ==> This is how you will define the functions (input can be optional based on the requirement)
+#            """ Here we should include the purpose of the function """     ==> This will help to idenity the purpose of the function (mandatory)
+#               contents_1
+#               contents_2
+#
+# When you call or declare a function, then you can use 'function_name' 
+# ()    - You can give input to function as a list when you declare this, if there is nothing to pass you can just leave () empty to satisfy the syntax  
+#
+#           function_name()                         ==> This is how you can call the functions, you can even give input in the paranethesis for the function as a list
+#           function_name(input_to_function)        ==> You can call function in this way also if you have a input or list of input to pass
+#
+# Example : Below is an example for defining function and giving a string input
+#
+#   | def check_availability(order): 
+#   |   """ This function is to check the order availability """
+#   |    if order == "burger":
+#   |       print "%s is available" %(order)
+#   |    elif order == "fries":
+#   |       print "%s is available" %(order)
+#   |    else:
+#   |       print "You order not available : %s" %(order)
+#   |
+#   | def check_order():
+#   |   """ This function will take user input as an order"""
+#   |   item_name = raw_input("Please enter your item : ")
+#   |   check_availability(item_name)
+#   |		
+#   | check_order()
+#
+# Example : Below example will show us the usage of calling a function with multiple parameters
+#
+# | def money_converter(money_amount, conversion_rate):                 => This function takes two parameter as inputs while defining
+# |    """ Does the conversion """
+# |    effective_rate = (money_amount * conversion_rate)
+# |    print "Your effective conversion rate is %d x %d = %d" %(money_amount, conversion_rate, effective_rate)
+# |
+# | def collect_user_input():
+# |   """ Takes user input """
+# |    money_amount = int(raw_input("Please enter the amount of money you have : "))
+# |    conversion_rate = int(raw_input("Please enter the conversion rate : "))
+# |    money_converter(money_amount, conversion_rate)                   => Here it declare the function with two parameter taken from user input
+# | 
+# | collect_user_input()
+#
+# * Importing a module 
+# In python there are amble amount of modules available which can used to perform various predefined actions
+# By using pre-defined modules, you are avoiding the duplication of code and can achieve your task simply by calling the function within th module 
+# Modules are called into the script by using a keyword 'import'and once modules are imported we can use functions within that modules according to their usage 
+#
+# Syntax : import module_name
+#          module_name.function_name ()     #  => You need to use this like module_name.function_name
+#
+# Example : Below example shows us the square root of a 'number'
+#
+# | import math
+# | number = 25
+# | result = math.sqrt(number)
+# | print "Your resuly is : %d" %(result)
+#
+# * Importing only a function from a module 
+# Lets say rather than using 'math.sqrt' function on the above example, you want to directly use it as 'sqrt()' function. 
+# To do that you can directly import that specific function from that module.
+# 
+# Syntax : from 'module_name' import 'function_name'
+#          function_name (variable)
+#
+# Example : Below example will show us how to only import sqrt function from math module
+#
+# | from math import sqrt
+# | 
+# | def sqrt_of_number(input_num):
+# |     """ This function finds the square root of a number """
+# |     if input_num:
+# |        square_root = sqrt(input_num)                              """ ==> Here we are using only sqrt() function and not calling the module """
+# |        print "Square root of %d : %d" %(input_num, square_root)
+# |
+# | number = int(raw_input("Please Enter your number :"))
+# | sqrt_of_number(number)
+#
+# * Importing all functions in a module 
+# If you want to import all functions from a module rather than only one function then you can use below synatx to call all the functions 
+#
+# syntax : from module_name import *
+#
+# NOTE : using this method is not advisable since there will be multiple functions from various modules may have same name that can cause confusion
+#        To avoid ending up in such situations it is always recomended that you will need to only use only below two method 
+#        1. use 'import module'  and then use 'module_name.function_name' method 
+#        2. use 'from module_name import function_name' method only to use the required function from that module 
+#
+#
+# * How to list all functions within a module 
+# To list all functions available with the module, you can use dir() function to convert all option as a list 
+# 
+# Example : synatx and example given below for further understanding
+#
+# | import math
+# | function_list = dir(math)
+# | print "\nList of functions available with module \'math\' are : \n\n %s" %(function_list)
+#
+# * Default or inbuilt functions in pythons
+# There are many inbuilt functions in python which you can use without importing any additional modules
+# You can get the complete details of these modules by referring to below link 
+# 
+# Source : https://docs.python.org/3/library/functions.html
+#
+#
+#
+#
+
+
