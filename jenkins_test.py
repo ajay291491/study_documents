2d87e2dd6f3dfb265ace0cd7d2c5b5a9a9c0e245
Ajayaghosh V L <ajay291491@gmail.com>
2018-03-31 00:32:49 +0000

    Update jenkins_test.py

--- jenkins_test.py
+++ jenkins_test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
 from datetime import datetime
 current_time = datetime.now()
-print current_time
+def display_date(date):
+  if date:
+    print date 
+    
+display_date(current_time.day)    
