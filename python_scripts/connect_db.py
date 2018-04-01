c38cc14182989c3f9a210f2306a78b40df6418cc
ajay291491 <ajay291491@gmail.com>
2018-04-01 14:37:00 +0800

    updating mariadb remore script

---
+++ python_scripts/connect_db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+# DESC : Script to manage monthly_finance table in MariaDB
+#
+
+#
+# Importing required modules 
+#
+
+import mysql.connector as mariadb
+from datetime import datetime
+
+#
+# Declaring variables 
+#
+
+current_time = datetime.now()
+
+#
+# Defining subroutines 
+#
+
+def get_car_models_available(manufacturer):
+  mariadb_connection = mariadb.connect(user='anu', password='anu275', host='rhcemaster01.svr.apac.sathsang.net', database='indian_cars')
+  cur = mariadb_connection.cursor()
+  query = ("SELECT car_name FROM suv where car_manufacturer='Tata'")
+  cur.execute(query)
+  for cars in cur:
+    print cars
+  mariadb_connection.close()
+
+#
+# Calling subroutine
+#
+
+get_car_models_available('Tata')
+
+#
+#
+# End
