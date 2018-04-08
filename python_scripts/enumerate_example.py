198bf1858ff832d0762cf0fc80d9e98a5ba00268
ajay291491 <ajay291491@gmail.com>
2018-04-09 07:35:17 +0800

    updating enumerate_example.py

---
+++ python_scripts/enumerate_example.py
@@ -0,0 +1,40 @@
+#!/usr/in/python
+#
+#
+#
+
+import subprocess
+import re
+
+#
+#
+#
+
+release_file  = '/etc/redhat-release'
+version_check = subprocess.Popen(["uname -r"], stdout = subprocess.PIPE, shell=True)
+(os_version, error) = version_check.communicate()
+
+#
+#
+
+def get_index_numbers(file):
+    
+    input_file = open(file, 'r')
+    for word in input_file.readlines():
+        split_words = word.split()
+        for (index_no, element) in enumerate(split_words):
+            print "Words in the file [ %s ] with element %d : %-5s" %(file, index_no, element)
+    input_file.close()
+
+#
+#
+#
+
+if re.search(r'^4\.11.+', os_version) is not None:
+    get_index_numbers(release_file)
+else:
+    print "You are not running on Redhat Linux and current kernel version is : %s" %(os_version)
+
+#
+#
+# End
