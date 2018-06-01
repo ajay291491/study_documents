bf0b4b233a4fcd8afdbe7cb3435e8138115a9860
ajay291491 <ajay291491@gmail.com>
2018-06-02 06:37:53 +0800

    updating python doc

--- begin_python.py
+++ begin_python.py
@@ -192,6 +192,125 @@
 # | #print choose_your_fruit('orange')
 # | user_input = raw_input("Please enter your fruit Choice : ")
 # | print "You have choosen : %s - " %(user_input) + choose_your_fruit(user_input)
+# * Types of Operator
+# Python language supports the following types of operators.
+# 
+#    . Arithmetic Operators
+#    . Comparison (Relational) Operators
+#    . Assignment Operators
+#    . Logical Operators
+#    . Bitwise Operators
+#    . Membership Operators
+#    . Identity Operators
+# 
+# Let us have a look on all operators one by one.
+# * Python Arithmetic Operators
+# Assume variable a holds 10 and variable b holds 20, then −
+# 
+# [ Show Example ]
+# Operator 		Description 								Example
+# + Addition 		Adds values on either side of the operator. 				a + b = 30
+# - Subtraction 		Subtracts right hand operand from left hand operand. 			a – b = -10
+# * Multiplication 	Multiplies values on either side of the operator 			a * b = 200
+# / Division 		Divides left hand operand by right hand operand 			b / a = 2
+# % Modulus 		Divides left hand operand by right hand operand and returns remainder 	b % a = 0
+# ** Exponent 		Performs exponential (power) calculation on operators 			a**b =10 to the power 20
+# 
+# These operators compare the values on either sides of them and decide the relation among them. They are also called Relational operators.
+# 
+# Assume variable a holds 10 and variable b holds 20, then −
+# 
+# [ Show Example ]
+# Operator 	Description 	Example
+# == 	If the values of two operands are equal, then the condition becomes true. 						(a == b) is not true.
+# != 	If values of two operands are not equal, then condition becomes true. 							(a != b) is true.
+# <> 	If values of two operands are not equal, then condition becomes true. 							(a <> b) is true. This is similar to != operator.
+# > 	If the value of left operand is greater than the value of right operand, then condition becomes true. 			(a > b) is not true.
+# < 	If the value of left operand is less than the value of right operand, then condition becomes true. 			(a < b) is true.
+# >= 	If the value of left operand is greater than or equal to the value of right operand, then condition becomes true. 	(a >= b) is not true.
+# <= 	If the value of left operand is less than or equal to the value of right operand, then condition becomes true. 		(a <= b) is true.
+# 
+# 
+# * Python Assignment Operators
+# Assume variable a holds 10 and variable b holds 20, then −
+# 
+# [ Show Example ]
+# Operator 			Description 											Example
+# = 				Assigns values from right side operands to left side operand 					c = a + b assigns value of a + b into c
+# += Add AND 			It adds right operand to the left operand and assign the result to left operand 		c += a is equivalent to c = c + a
+# -= Subtract AND 		It subtracts right operand from the left operand and assign the result to left operand 		c -= a is equivalent to c = c - a
+# *= Multiply AND 		It multiplies right operand with the left operand and assign the result to left operand 	c *= a is equivalent to c = c * a
+# /= Divide AND 			It divides left operand with the right operand and assign the result to left operand 		c /= a is equivalent to c = c / ac /= a is equivalent to c = c / a
+# %= Modulus AND 			It takes modulus using two operands and assign the result to left operand 			c %= a is equivalent to c = c % a
+# **= Exponent AND 		Performs exponential (power) calculation on operators and assign value to the left operand 	c **= a is equivalent to c = c ** a
+# //= Floor Division 		It performs floor division on operators and assign value to the left operand 			c //= a is equivalent to c = c // a
+# 
+# * Python Bitwise Operators
+# Bitwise operator works on bits and performs bit by bit operation. Assume if a = 60; and b = 13; Now in binary format they will be as follows −
+# a = 0011 1100
+# b = 0000 1101
+# -----------------
+# a&b = 0000 1100
+# a|b = 0011 1101
+# a^b = 0011 0001
+# ~a  = 1100 0011
+# 
+# There are following Bitwise operators supported by Python language
+# 
+# [ Show Example ]
+# Operator 			Description 	Example
+# & Binary AND 			Operator copies a bit to the result if it exists in both operands 	(a & b) (means 0000 1100)
+# | Binary OR 			It copies a bit if it exists in either operand. 	(a | b) = 61 (means 0011 1101)
+# ^ Binary XOR 			It copies the bit if it is set in one operand but not both. 	(a ^ b) = 49 (means 0011 0001)
+# ~ Binary Ones Complement 	It is unary and has the effect of 'flipping' bits. 	(~a ) = -61 (means 1100 0011 in 2's complement form due to a signed binary number.
+# << Binary Left Shift 		The left operands value is moved left by the number of bits specified by the right operand. 	a << 2 = 240 (means 1111 0000)
+# >> Binary Right Shift 		The left operands value is moved right by the number of bits specified by the right operand. 	a >> 2 = 15 (means 0000 1111)
+# 
+# 
+# * Python Logical Operators
+# There are following logical operators supported by Python language. Assume variable a holds 10 and variable b holds 20 then
+# 
+# [ Show Example ]
+# Operator 		Description 								Example
+# and Logical AND 	If both the operands are true then condition becomes true. 		(a and b) is true.
+# or Logical OR 		If any of the two operands are non-zero then condition becomes true. 	(a or b) is true.
+# not Logical NOT 	Used to reverse the logical state of its operand. 			Not(a and b) is false.
+# 
+# * Membership Operators
+# Python’s membership operators test for membership in a sequence, such as strings, lists, or tuples. There are two membership operators as explained below −
+# 
+# [ Show Example ]
+# Operator 	Description 											Example
+# in 		Evaluates to true if it finds a variable in the specified sequence and false otherwise. 	x in y, here in results in a 1 if x is a member of sequence y.
+# not in 		Evaluates to true if it does not finds a variable in aspecified sequence and false otherwise.	x not in y, here not in results in a 1 if x is not a member of sequence y.
+# 
+# * Python Identity Operators
+# Identity operators compare the memory locations of two objects. There are two Identity operators explained below −
+# 
+# [ Show Example ]
+# Operator 	Description 														Example
+# is 		Evaluates to true if the variables on either side of the operator point to the same object and false otherwise. 	x is y, here is results in 1 if id(x) equals id(y).
+# is not 		Evaluates to false if the variables on either side of the operator point to the same object and true otherwise. 	x is not y, here is not results in 1 if id(x) is not equal to id(y).
+# 
+# 
+# * Python Operators Precedence
+# The following table lists all operators from highest precedence to lowest.
+# 
+# [ Show Example ]
+# Sr.No. 	Operator & Description
+# 1 	**			Exponentiation (raise to the power)
+# 2 	~ + -			Complement, unary plus and minus (method names for the last two are +@ and -@)
+# 3 	* / % //		Multiply, divide, modulo and floor division
+# 4 	+ -			Addition and subtraction
+# 5 	>> <<			Right and left bitwise shift
+# 6 	&			Bitwise 'AND'
+# 7 	^ |			Bitwise exclusive `OR' and regular `OR'
+# 8 	<= < > >=		Comparison operators
+# 9 	<> == !=		Equality operators
+# 10 	= %= /= //= -= += *= **=Assignment operators
+# 11 	is is not		Identity operators
+# 12 	in not in		Membership operators
+# 13 	not or and		Logical operators
 #      
 #-------------------------------------------------------------------------------------------------------------
 # Chapter 03 - inputs
@@ -1916,6 +2035,93 @@
 # | process_requests(family)
 # |
 #
+# * Reading the response content
+# Requests itself will talk to the server and will decode the content, most unicode contents are seamlessly decoded. 
+# Requests uses the HTTP headers to make the encoding guesses. 
+# You can get and set the default encoding using the below synatx. 
+#
+# Syntax : object.encodig                       => This will get the encoding value 
+#        : object.encoding = 'encoding type'    => This will help you to set the encoding 
+#
+# Request can rereive the data in various different formats such as text, json, raw and binary, their syntax given below 
+#
+# Syntax : object.text          -> This will return the contect in a text format
+#        : object.raw           -> This will not format the data in any format, it just dump the source data 
+#        : object.json()        -> This will return the data in a json format if the source support the same
+#        : object.content       -> If you have any binary content, then mostly you can use the content method to retrieve the data 
+#
+# Example : Below example will show you how to get and set the encoding 
+#
+# | from pprint import pprint 
+# | import requests
+# | github_url = 'https://api.github.com/events'
+# | connection = requests.get(github_url)
+# | print connection.encoding
+# | connection.encoding = 'ASCII'
+# | print connection.encoding
+# | pprint (connection.json())
+# | print connection.text
+# | print connection.raw
+# | print connection.content
+#
+# * Custom Headers 
+# You can specify custom headers along with the request with a keyword 'headers' 
+# First you will need to create a dictionary with the header infromation and then will need to pass that dictionary to the 'headers' parameter
+# 
+# Syntax : header_info = { 'key1': 'value1', 
+#                          'key2': 'value2'}
+#          content = requests.get('https://someurl.com', headers=header_info)
+#
+# You will need to note below points when you are using custom headers 
+# - Custom headers are given less precedence than the more specific information
+# - Autherization headers will be overriden if there is parameters specified in '.netrc' which inturn overriden by the auth parametr
+# - Autherization parameter will be ignored if you get redirected from the host
+# - Proxy authorization headers will be overriden by the proxy credentials provided in the url 
+# - Content length headers will be overriden when we can determine the length of the content
+#
+# * Response headers 
+# You can view the http header content from the url using below syntax 
+# 
+# Syntax  : object.headers
+#
+# Example : Below example will show us the usage of header 
+#
+# | import re 
+# | import requests
+# | from pprint import pprint 
+# |
+# | url = 'https://api.github.com/events'
+# | header_info = {'useragent': 'my-app/0.0.1'}
+# | content = requests.get(url, headers=header_info)
+# | if content.status_code == 403 : 
+# |     print "failed to gather info"
+# | else: 
+# |    for keys in content.headers:
+# |         link_check = re.search(r'^<https://\w.+\.\w{3}', content.headers[keys], re.I|re.M)
+# |         if link_check: print "Link : %s" %(link_check.group())
+# |         else: print "Do not found use agent"
+# 
+# * More complicated POST requests
+# According to the catagory we can consider post request into different types according to the data itself 
+#
+# - Data like HTML  : If you have data like HTML then you can simply pass the the contents to the 'data', request will automatically take care of the encoding
+#
+# | import requests 
+# | upload = {'tool': 'puppet'}
+# | url = 'http://httpbin.org'
+# | connect = requests.post(url + '/post', data=upload)
+# | print connect.content
+#
+# - List and Tuples : This also can be passed aling with the 'data' argument and request will take care of its argument
+#
+# | import requests
+# | url = 'http://httpbin.org'
+# | upload = (('key1', 'value1'), ('key2', 'value2')) 
+# | connect = requests.post(url + '/post', data = upload)
+# | print connect.content
+#
+# - Json format - [ Continue from there ] 
+#
 # NOTE : Continue from http://docs.python-requests.org/en/master/user/quickstart/#
 #
 #-------------------------------------------------------------------------------------------------------------
@@ -1965,5 +2171,6 @@
 # Chapter 17 - Writing test cases in python
 #-------------------------------------------------------------------------------------------------------------
 #
+# https://jeffknupp.com/blog/2013/12/09/improve-your-python-understanding-unit-testing/
 #
 #-------------------------------------------------------------------------------------------------------------
